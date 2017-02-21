84e2d5c68d907e43b8132a65d30f74830da5cd4c
dhavalpathak <dhaval_pathak2001@yahoo.com>
2017-02-20 22:44:34 -0500

    Add project files.

---
+++ Database/Tables/Doctor.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Doctor]
+(
+	[Id]			INT NOT NULL PRIMARY KEY IDENTITY(1000, 1),
+	[FirstName]		varchar(255) NOT NULL,
+	[LastName]		varchar(255) NOT NULL,
+	[Address1]		varchar(max) NOT NULL,
+	[Address2]		varchar(max),
+	[Province]		Varchar(2) NOT NULL,
+	[Email]			varchar(100) NOT NULL,
+	[Phone]			varchar(10) NOT NULL
+)
