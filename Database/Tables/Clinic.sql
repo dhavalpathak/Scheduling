854177922c7aedfa3bdcad847b9d8df3ae0d7547
dhavalpathak <dhaval_pathak2001@yahoo.com>
2017-03-13 21:58:37 -0400

    added clinic table

--- Database/Tables/Clinic.sql
+++ Database/Tables/Clinic.sql
@@ -1,8 +1,7 @@
-﻿CREATE TABLE [dbo].[Doctor]
+﻿CREATE TABLE [dbo].[Clinic]
 (
 	[Id]			INT NOT NULL PRIMARY KEY IDENTITY(1000, 1),
-	[FirstName]		varchar(255) NOT NULL,
-	[LastName]		varchar(255) NOT NULL,
+	[Name]			varchar(255) NOT NULL,
 	[Address1]		varchar(max) NOT NULL,
 	[Address2]		varchar(max),
 	[PostalCode]	nchar(6) NOT NULL,
